c3d2b2ef61d2ce3a2365b806b6fcb38d042befaf
ndakic <nikoladakic@uns.ac.rs>
2022-01-20 21:06:59 +0100

    random generator fix

--- util/data_util.py
+++ util/data_util.py
@@ -7,17 +7,16 @@ COLUMN_NAMES = ["id", "name", "album", "album_id", "artists", "artist_ids", "tra
                 "liveness", "valence", "tempo", "duration_ms", "time_signature", "year", "release_date"]
 
 
-def generate_random_user_likes(filename, total_number_of_tracks, total_number_of_users):
-    data = load_data(filename, True)
+def generate_random_user_likes(dataset, target_file_name, total_number_of_tracks, total_number_of_users):
     results = []
     for i in range(0, total_number_of_users):
-        random_tracks = data.sample(total_number_of_tracks)[["id"]]
+        random_tracks = dataset.sample(total_number_of_tracks)[["id"]]
         random_tracks = random_tracks.rename(columns={'id': 'track_id'})
         random_tracks['user_id'] = i+1
         results.append(random_tracks)
 
     final_result = pd.concat(results)
-    final_result.to_csv("data/liked_tracks.csv", columns=["user_id", "track_id"], index=False)
+    final_result.to_csv(target_file_name, columns=["user_id", "track_id"], index=False)
     return final_result
 
 
@@ -30,7 +29,7 @@ def load_data(filename, raw=False):
     if raw:
         return data
     selected_features = data.drop(columns=['id', 'release_date', 'year', 'album', 'album_id', 'artist_ids'])
-    print("Loaded data (10 RAW samples): \n", selected_features.sample(10))
+    print("Loaded data (10 samples): \n", selected_features.sample(10))
     return selected_features
 
 
