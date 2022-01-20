34ce48dd4cd8d31d9528911b9286875e2cbe8c53
ndakic <nikoladakic@uns.ac.rs>
2022-01-20 22:46:21 +0100

    user_id changed

--- util/data_util.py
+++ util/data_util.py
@@ -12,7 +12,7 @@ def generate_random_user_likes(dataset, target_file_name, total_number_of_tracks
     for i in range(0, total_number_of_users):
         random_tracks = dataset.sample(total_number_of_tracks)[["id"]]
         random_tracks = random_tracks.rename(columns={'id': 'track_id'})
-        random_tracks['user_id'] = i+1
+        random_tracks['user_id'] = "user-{}".format(i+1)
         results.append(random_tracks)
 
     final_result = pd.concat(results)
@@ -24,8 +24,10 @@ def print_random_samples(data):
     print("10 random samples from dataset: \n", data.sample(10))
 
 
-def load_data(filename, raw=False):
-    data = pd.read_csv(filename, skiprows=[0], sep=",", names=COLUMN_NAMES, low_memory=False)
+def load_data(filename, raw=False, column_names=False):
+    if not column_names:
+        column_names = COLUMN_NAMES
+    data = pd.read_csv(filename, skiprows=[0], sep=",", names=column_names, low_memory=False)
     if raw:
         return data
     selected_features = data.drop(columns=['id', 'release_date', 'year', 'album', 'album_id', 'artist_ids'])
